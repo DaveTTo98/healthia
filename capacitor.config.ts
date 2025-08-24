7798e84e319fd11baf04f15213c381d93d3a5397
DAvid <david.vt.sb@gmail.com>
2025-08-23 23:46:15 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'healthia',
+  webDir: 'www'
+};
+
+export default config;
